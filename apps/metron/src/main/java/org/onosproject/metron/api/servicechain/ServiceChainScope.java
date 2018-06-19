3be7bdb01c79fe781799b6d03ad5dbb4d4503325
Georgios Katsikas <katsikas.gp@gmail.com>
2018-06-19 09:06:13 +0200

    [Metron-Ctrl] Service chain scope has been made more specific
    Change-Id: I472856f35f1c49f02167028f87679517785d68e1
    Signed-off-by: Georgios Katsikas <katsikas.gp@gmail.com>

--- apps/metron/src/main/java/org/onosproject/metron/api/servicechain/ServiceChainScope.java
+++ apps/metron/src/main/java/org/onosproject/metron/api/servicechain/ServiceChainScope.java
@@ -16,6 +16,9 @@
 
 package org.onosproject.metron.api.servicechain;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Scope of a service chain.
  */
@@ -23,18 +26,41 @@ public enum ServiceChainScope {
 
     /**
      * Service chain deployment scopes:
-     * |-> SERVER indicates that no network
-     *     element will be used for offloading.
-     *     Instead, server's NIC(s) will be used for
-     *     classification, tagging, and dispatching.
-     * |-> NETWORK indicates that available network
+     * |-> NETWORK_MAC indicates that available network
      *     elements will be exploited for classification
-     *     and tagging.
+     *     and tagging. These elements will use the destination
+     *     MAC address as a field for tagging.
+     * |-> NETWORK_VLAN indicates that available network
+     *     elements will be exploited for classification
+     *     and tagging. These elements will use the VLAN
+     *     VID as a field for tagging.
+     * |-> SERVER_RULES indicates that no network
+     *     element will be used for offloading.
+     *     Instead, server's NIC(s) will be programmed with
+     *     explicit rules for classification and dispatching.
+     * |-> SERVER_RSS indicates that no network
+     *     element will be used for offloading.
+     *     Instead, server's NIC(s) will utilize Receive-Side
+     *     Scaling (RSS) to dispatch input traffic.
+     *     In this case, traffic classification is entirely
+     *     implemented in software.
      */
-    SERVER("server"),
-    NETWORK("network");
+    NETWORK_MAC("network-mac"),
+    NETWORK_VLAN("network-vlan"),
+    SERVER_RULES("server-rules"),
+    SERVER_RSS("server-rss");
 
     private String scScope;
+
+    // Statically maps service chain scopes to enum types
+    private static final Map<String, ServiceChainScope> MAP =
+        new ConcurrentHashMap<String, ServiceChainScope>();
+
+    static {
+        for (ServiceChainScope scope : ServiceChainScope.values()) {
+            MAP.put(scope.toString().toLowerCase(), scope);
+        }
+    }
 
     private ServiceChainScope(String scScope) {
         this.scScope = scScope.toLowerCase();
@@ -52,11 +78,17 @@ public enum ServiceChainScope {
      * @return boolean validity status
      */
     public static boolean isValid(ServiceChainScope scScope) {
-        if ((scScope != SERVER) && (scScope != NETWORK)) {
-            return false;
-        }
+        return MAP.get(scScope.toString().toUpperCase()) != null;
+    }
 
-        return true;
+    /**
+     * Returns whether a service chain's scope is valid or not.
+     *
+     * @param scScopeStr the service chain's scope as a string
+     * @return boolean validity status
+     */
+    public static boolean isValid(String scScopeStr) {
+        return MAP.get(scScopeStr.toUpperCase()) != null;
     }
 
     /**
@@ -66,7 +98,7 @@ public enum ServiceChainScope {
      * @return boolean status
      */
     public static boolean isServerLevel(ServiceChainScope scScope) {
-        return scScope == SERVER;
+        return (scScope.toString().contains("server"));
     }
 
     /**
@@ -76,7 +108,30 @@ public enum ServiceChainScope {
      * @return boolean status
      */
     public static boolean isNetworkWide(ServiceChainScope scScope) {
-        return scScope == NETWORK;
+        return (scScope.toString().contains("network"));
+    }
+
+    /**
+     * Returns whether a service chain's scope is software-based or not.
+     * SERVER_RSS is a dispatching mechanism, hence it is considered
+     * software-based because it requires additional traffic
+     * classification in software.
+     *
+     * @param scScope the service chain's scope
+     * @return boolean status
+     */
+    public static boolean isSoftwareBased(ServiceChainScope scScope) {
+        return scScope == SERVER_RSS;
+    }
+
+    /**
+     * Returns whether a service chain's scope is hardware-based or not.
+     *
+     * @param scScope the service chain's scope
+     * @return boolean status
+     */
+    public static boolean isHardwareBased(ServiceChainScope scScope) {
+        return !isSoftwareBased(scScope);
     }
 
 }
