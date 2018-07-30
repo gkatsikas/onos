ca1e90a6358a098d62721b02fba1feda7fef283e
Georgios Katsikas <katsikas.gp@gmail.com>
2018-07-30 22:33:29 +0200

    [Metron-Ctrl] Refactored service chain stats UI
    Change-Id: I7f24957bfe769f111a5174d72c1e62c62c7241de
    Signed-off-by: Georgios Katsikas <katsikas.gp@gmail.com>

--- apps/metron/src/main/java/org/onosproject/metron/impl/gui/Resource.java
+++ apps/metron/src/main/java/org/onosproject/metron/impl/gui/Resource.java
@@ -33,33 +33,11 @@ public final class Resource {
     private Resource() {}
 
     /**
-     * Resource type.
-     */
-    public enum Type {
-        /* Server-related metrics */
-        SERVER,
-
-        /* Service chain-related metrics */
-        SERVICE_CHAIN
-    }
-
-    /**
      * A collection of metrics related to the deployment of service chains.
      */
     public static final Set<MetricType> SERVICE_CHAIN_DEPL_METRICS =
         ImmutableSet.of(
             SYNTHESIS_TIME, DEPLOYMENT_TIME, MONITORING_TIME, RECONFIGURATION_TIME
-        );
-
-    /**
-     * A collection of metrics related to the CPU load of an NFV server.
-     */
-    public static final Set<MetricType> CPU_CORES_METRICS =
-        ImmutableSet.of(
-            MetricType.CPU_0,  MetricType.CPU_1,  MetricType.CPU_2,  MetricType.CPU_3,
-            MetricType.CPU_4,  MetricType.CPU_5,  MetricType.CPU_6,  MetricType.CPU_7,
-            MetricType.CPU_8,  MetricType.CPU_9,  MetricType.CPU_10, MetricType.CPU_11,
-            MetricType.CPU_12, MetricType.CPU_13, MetricType.CPU_14, MetricType.CPU_15
         );
 
 }
